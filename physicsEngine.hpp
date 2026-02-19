24f26210f3e0772e3350317b5b343e61c94c1d44
JTechGaming <piepnieuweboer@gmail.com>
2026-02-19 11:51:46 +0100

    almost done with physics engine

--- physicsEngine.hpp
+++ physicsEngine.hpp
@@ -10,6 +10,38 @@ struct Object {
     float mass;
 };
 
+class PhysicsScene {
+private:
+    std::vector<Object*> _objects;
+    glm::vec3 _gravity = glm::vec3(0, -9.81f, 0);
+public:
+    void SpawnObject(Object* object) { }
+    void DeleteObject(Object* object) { }
+
+    void Tick(float dt) {
+        for (Object* obj : _objects) {
+            obj->force += obj->mass * _gravity;
+            obj->velocity += obj->force / obj->mass * dt;
+            obj->position += obj->velocity * dt;
+            obj->force = glm::vec3(0, 0, 0);
+        }
+    }
+};
+
+struct CollisionPoints {
+    glm::vec3 A;
+    glm::vec3 B;
+    glm::vec3 normal;
+    glm::vec3 depth;
+    glm::vec3 hasCollision;
+};
+
+struct Transform {
+    glm::vec3 position;
+    glm::vec3 rotation;
+    glm::vec3 scale;
+};
+
 struct Collider {
     virtual glm::vec3 FindFurthestPoint(glm::vec3 direction) const = 0;
 };
@@ -178,6 +210,131 @@ bool runGJKCheck(const Collider& A, const Collider& B) {
     }
 }
 
-CollisionPoints EPA() {
+std::pair<std::vector<glm::vec4>, size_t> GetFaceNormals(
+    const std::vector<glm::vec3>& polytope, const std::vector<size_t>& faces
+) {
+    std::vector<glm::vec4> normals;
+    size_t minTriangle = 0;
+    float minDistance = FLT_MAX;
+
+    for (size_t i = 0; i < faces.size(); i += 3) {
+        glm::vec3 a = polytope[faces[i]];
+        glm::vec3 b = polytope[faces[i+1]];
+        glm::vec3 c = polytope[faces[i+2]];
+        glm::vec3 normal = glm::normalize(glm::cross(b-a, c-a));
+        float distance = glm::dot(normal, a);
+        if (distance < 0) {
+            normal *= -1;
+            distance *= -1;
+        }
+
+        normals.emplace_back(normal, distance);
+
+        if (distance < minDistance) {
+            minTriangle = i / 3;
+            minDistance = distance;
+        }
+    }
+
+    return { normals, minTriangle };
+}
+
+void AddIfUniqueEdge(
+    std::vector<std::pair<size_t, size_t>>& edges,
+    const std::vector<size_t>& faces,
+    size_t a, size_t b
+) {
+    auto reverse = std::find(
+        edges.begin(),
+        edges.end(),
+        std::make_pair(faces[b], faces[a])
+    );
     
+    if (reverse != edges.end()) {
+        edges.erase(reverse);
+    } else {
+        edges.emplace_back(faces[a], faces[b]);
+    }
+}
+
+CollisionPoints EPA(const Simplex& simplex, Collider& A, Collider& B) {
+    std::vector<glm::vec3> polytope(simplex.begin(), simplex.end());
+    std::vector<size_t> faces = {
+        0, 1, 2,
+        0, 3, 1,
+        0, 2, 3,
+        1, 3, 2
+    };
+
+    auto [normals, minFace] = GetFaceNormals(polytope, faces);
+
+    glm::vec3 minNormal;
+    float minDistance = FLT_MAX;
+
+    while (minDistance == FLT_MAX) {
+        minNormal = normals[minFace].xyz();
+        minDistance = normals[minFace].w;
+
+        glm::vec3 support = Support(A, B, minNormal);
+        float sDistance = glm::dot(minNormal, support);
+
+        if (abs(sDistance - minDistance) > 0.001f) {
+            minDistance = FLT_MAX;
+
+            std::vector<std::pair<size_t, size_t>> uniqueEdges;
+            for (size_t i = 0; i < normals.size(); i++) {
+                if (SameDirection(normals[i], support)) {
+                    size_t f = i*3;
+                    AddIfUniqueEdge(uniqueEdges, faces, f, f+1);
+                    AddIfUniqueEdge(uniqueEdges, faces, f+1, f+2);
+                    AddIfUniqueEdge(uniqueEdges, faces, f+2, f);
+
+                    faces[f+2] = faces.back();
+                    faces.pop_back();
+                    faces[f+1] = faces.back();
+                    faces.pop_back();
+                    faces[f] = faces.back();
+                    faces.pop_back();
+
+                    normals[i] = normals.back();
+                    normals.pop_back();
+
+                    i--;
+                }
+            }
+
+            std::vector<size_t> newFaces;
+            for (auto[edgeIndex1, edgeIndex2] : uniqueEdges) {
+                newFaces.push_back(edgeIndex1);
+                newFaces.push_back(edgeIndex2);
+                newFaces.push_back(polytope.size());
+            }
+
+            polytope.push_back(support);
+
+            auto[newNormals, newMinFace] = GetFaceNormals(polytope, newFaces);
+
+            float oldMinDistance = FLT_MAX;
+            for (size_t i = 0; i < normals.size(); i++) {
+                if (normals[i].w < oldMinDistance) {
+                    oldMinDistance = normals[i].w;
+                    minFace = i;
+                }
+            }
+
+            if (newNormals[newMinFace].w < oldMinDistance) {
+                minFace = newMinFace + normals.size();
+            }
+
+            faces.insert(faces.end(), newFaces.begin(), newFaces.end());
+            normals.insert(normals.end(), newNormals.begin(), newNormals.end());
+        }
+    }
+
+    CollisionPoints points;
+    points.normal = minNormal;
+    points.penetrationDepth = minDistance + 0.001f;
+    points.hasCollision = true;
+
+    return points;
 }
